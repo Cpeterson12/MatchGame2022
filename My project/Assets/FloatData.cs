0e4bc84cab1bc4319f26ca67663733c6275e5188
caden <cpete.est2014@gmail.com>
2022-09-07 15:43:11 -0600

    Added HW from 9/7

---
+++ My project/Assets/FloatData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+//floatdata is inherited from scriptableobject
+public class FloatData : ScriptableObject
+{
+  public float value;
+
+  public void SetValue(float num)
+  {
+    value = num;
+  }
+  
+  public void UpdateValue(float num)//num is the updated value
+  {
+    value += num;
+  }
+}
+
